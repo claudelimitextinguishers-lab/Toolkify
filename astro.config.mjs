ab438977764ba09f726566762bb0dcd1e08e0964
Arjav Jain <arjav.jain.one@gmail.com>
2026-07-11 03:19:29 +0530

    fixed stiemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,22 @@
-﻿// @ts-check
+// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://toolkify.com',
+  integrations: [
+    sitemap({
+      filter: (page) =>
+        !page.includes('/403') &&
+        !page.includes('/404') &&
+        !page.includes('/500'),
+      changefreq: 'weekly',
+      priority: 0.8,
+      lastmod: new Date(),
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
